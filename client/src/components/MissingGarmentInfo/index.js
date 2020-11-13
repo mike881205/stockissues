1715b37f6958bc5fad4bcd94aba8d5e882a8c6fc
mike881205 <mike881205@gmail.com>
2020-11-12 21:27:11 -0500

    moved all change handle functions from missing component to parent home component. Updated conditional rendering to display reason drop down after selecting an issue

--- client/src/components/MissingGarmentInfo/index.js
+++ client/src/components/MissingGarmentInfo/index.js
@@ -5,208 +5,6 @@ import { DropdownList } from "react-widgets"
 
 class MissingGarmentInfo extends React.Component {
 
-    state = {
-        brand: "",
-        style: "",
-        color: "",
-        size: {
-            xSmall: 0,
-            small: 0,
-            medium: 0,
-            large: 0,
-            xLarge: 0,
-            twoXL: 0,
-            threeXL: 0,
-            fourXL: 0,
-            fiveXL: 0,
-        },
-        hats: 0
-    }
-
-    handleInputChange = event => {
-
-        const { name, value } = event.target;
-
-        if (name === "color") {
-
-            let colorVal = value.replace(/[^a-zA-Z ]/g, "")
-
-            this.setState({ color: colorVal.trim() });
-        }
-        else {
-            this.setState({ [name]: value.trim() });
-        }
-
-        // let sizeArray = [
-        //     this.state.size.xSmall,
-        //     this.state.size.small,
-        //     this.state.size.medium,
-        //     this.state.size.large,
-        //     this.state.size.xLarge,
-        //     this.state.size.twoXL,
-        //     this.state.size.threeXL,
-        //     this.state.size.fourXL,
-        //     this.state.size.fiveXL
-        // ]
-
-        // let greaterThanZero = false
-        // let brandStyleColor = false
-        // let brand = this.state.brand
-        // let style = this.state.style
-        // let color = this.state.color
-
-        // if (name === "color") {
-
-        //     let colorVal = value.replace(/[^a-zA-Z ]/g, "")
-
-        //     if (value.length > 0) {
-        //         this.setState({ color: colorVal.trim() });
-        //     }
-        //     else {
-        //         this.setState({ color: "" });
-        //     }
-        // }
-        // else {
-
-        //     if (value.length > 0) {
-        //         this.setState({ [name]: value.trim() });
-        //     }
-        //     else {
-        //         this.setState({ [name]: "" });
-        //     }
-        // }
-
-        // for (let i = 0; i < sizeArray.length; i++) {
-
-        //     let size = sizeArray[i]
-
-        //     if (size > 0) {
-        //         greaterThanZero = true
-        //         console.log("Greater than Zero")
-        //     }
-        // }
-
-        // if (brand !== "" && style !== "" && color !== "") {
-        //     brandStyleColor = true
-
-        //     console.log("brandStyleColor")
-        // }
-
-        // if (greaterThanZero && brandStyleColor) {
-
-        //     console.log("Info Complete")
-
-        //     this.props.setMissingState(this.state)
-        // }
-        // else {
-        //     console.log("Info Inomplete")
-        // }
-    };
-
-    sizeInputChange = event => {
-
-        const { name, value } = event.target;
-
-        let numVal = parseInt(value)
-
-        let sizeObj = {
-            xSmall: this.state.size.xSmall,
-            small: this.state.size.small,
-            medium: this.state.size.medium,
-            large: this.state.size.large,
-            xLarge: this.state.size.xLarge,
-            twoXL: this.state.size.twoXL,
-            threeXL: this.state.size.threeXL,
-            fourXL: this.state.size.fourXL,
-            fiveXL: this.state.size.fiveXL
-        }
-
-        let hats = this.state.hats
-
-        switch (name) {
-            case "xSmall":
-                sizeObj.xSmall = numVal
-                break;
-            case "small":
-                sizeObj.small = numVal
-                break;
-            case "medium":
-                sizeObj.medium = numVal
-                break;
-            case "large":
-                sizeObj.large = numVal
-                break;
-            case "xLarge":
-                sizeObj.xLarge = numVal
-                break;
-            case "twoXL":
-                sizeObj.twoXL = numVal
-                break;
-            case "threeXL":
-                sizeObj.threeXL = numVal
-                break;
-            case "fourXL":
-                sizeObj.fourXL = numVal
-                break;
-            case "fiveXL":
-                sizeObj.fiveXL = numVal
-                break;
-            case "hats":
-                hats = numVal
-                break;
-        }
-
-        this.setState({ 
-            size: sizeObj,
-            hats: hats 
-        })
-
-        // let greaterThanZero = false
-        // let brandStyleColor = false
-
-        // let brand = this.state.brand
-        // let style = this.state.style
-        // let color = this.state.color
-
-        // let sizeArray = [
-        //     sizeObj.xSmall,
-        //     sizeObj.small,
-        //     sizeObj.medium,
-        //     sizeObj.large,
-        //     sizeObj.xLarge,
-        //     sizeObj.twoXL,
-        //     sizeObj.threeXL,
-        //     sizeObj.fourXL,
-        //     sizeObj.fiveXL
-        // ]
-
-        // for (let i = 0; i < sizeArray.length; i++) {
-
-        //     let size = sizeArray[i]
-
-        //     if (size > 0) {
-        //         greaterThanZero = true
-        //         console.log("greaterThanZero")
-        //     }
-        // }
-
-        // if (brand !== "" && style !== "" && color !== "") {
-        //     brandStyleColor = true
-
-        //     console.log("brandStyleColor")
-        // }
-
-        // if (greaterThanZero && brandStyleColor) {
-
-        //     console.log("Info Complete")
-
-        //     this.props.setMissingState(this.state)
-        // }
-        // else {
-        //     console.log("Info Inomplete")
-        // }
-    }
-
     render() {
         return (
             <FormGroup>
@@ -216,8 +14,8 @@ class MissingGarmentInfo extends React.Component {
                             <Label text="Brand" />
                             <Input
                                 name="brand"
-                                value={this.state.brand}
-                                onChange={this.handleInputChange}
+                                value={this.props.brand}
+                                onChange={this.props.handleInputChange}
                             />
                         </FormGroup>
                     </div>
@@ -226,8 +24,8 @@ class MissingGarmentInfo extends React.Component {
                             <Label text="Style Number" />
                             <Input
                                 name="style"
-                                value={this.state.style}
-                                onChange={this.handleInputChange}
+                                value={this.props.style}
+                                onChange={this.props.handleInputChange}
                             />
                         </FormGroup>
                     </div>
@@ -236,8 +34,8 @@ class MissingGarmentInfo extends React.Component {
                             <Label text="Color" />
                             <Input
                                 name="color"
-                                value={this.state.color}
-                                onChange={this.handleInputChange}
+                                value={this.props.color}
+                                onChange={this.props.handleInputChange}
                             />
                         </FormGroup>
                     </div>
@@ -247,8 +45,8 @@ class MissingGarmentInfo extends React.Component {
                         <Label text="XS" />
                         <Input
                             name="xSmall"
-                            value={this.state.size.xSmall}
-                            onChange={this.sizeInputChange}
+                            value={this.props.xSmall}
+                            onChange={this.props.handleInputChange}
                             type="number"
                             min={0}
                             onKeyDown={(event) => {
@@ -260,8 +58,8 @@ class MissingGarmentInfo extends React.Component {
                         <Label text="S" />
                         <Input
                             name="small"
-                            value={this.state.size.small}
-                            onChange={this.sizeInputChange}
+                            value={this.props.small}
+                            onChange={this.props.handleInputChange}
                             type="number"
                             min={0}
                             onKeyDown={(event) => {
@@ -273,8 +71,8 @@ class MissingGarmentInfo extends React.Component {
                         <Label text="M" />
                         <Input
                             name="medium"
-                            value={this.state.size.medium}
-                            onChange={this.sizeInputChange}
+                            value={this.props.medium}
+                            onChange={this.props.handleInputChange}
                             type="number"
                             min={0}
                             onKeyDown={(event) => {
@@ -286,8 +84,8 @@ class MissingGarmentInfo extends React.Component {
                         <Label text="L" />
                         <Input
                             name="large"
-                            value={this.state.size.large}
-                            onChange={this.sizeInputChange}
+                            value={this.props.large}
+                            onChange={this.props.handleInputChange}
                             type="number"
                             min={0}
                             onKeyDown={(event) => {
@@ -299,8 +97,8 @@ class MissingGarmentInfo extends React.Component {
                         <Label text="XL" />
                         <Input
                             name="xLarge"
-                            value={this.state.size.xLarge}
-                            onChange={this.sizeInputChange}
+                            value={this.props.xLarge}
+                            onChange={this.props.handleInputChange}
                             type="number"
                             min={0}
                             onKeyDown={(event) => {
@@ -312,8 +110,8 @@ class MissingGarmentInfo extends React.Component {
                         <Label text="2XL" />
                         <Input
                             name="twoXL"
-                            value={this.state.size.twoXL}
-                            onChange={this.sizeInputChange}
+                            value={this.props.twoXL}
+                            onChange={this.props.handleInputChange}
                             type="number"
                             min={0}
                             onKeyDown={(event) => {
@@ -325,8 +123,8 @@ class MissingGarmentInfo extends React.Component {
                         <Label text="3XL" />
                         <Input
                             name="threeXL"
-                            value={this.state.size.threeXL}
-                            onChange={this.sizeInputChange}
+                            value={this.props.threeXL}
+                            onChange={this.props.handleInputChange}
                             type="number"
                             min={0}
                             onKeyDown={(event) => {
@@ -338,8 +136,8 @@ class MissingGarmentInfo extends React.Component {
                         <Label text="4XL" />
                         <Input
                             name="fourXL"
-                            value={this.state.size.fourXL}
-                            onChange={this.sizeInputChange}
+                            value={this.props.fourXL}
+                            onChange={this.props.handleInputChange}
                             type="number"
                             min={0}
                             onKeyDown={(event) => {
@@ -351,8 +149,8 @@ class MissingGarmentInfo extends React.Component {
                         <Label text="5XL" />
                         <Input
                             name="fiveXL"
-                            value={this.state.size.fiveXL}
-                            onChange={this.sizeInputChange}
+                            value={this.props.fiveXL}
+                            onChange={this.props.handleInputChange}
                             type="number"
                             min={0}
                             onKeyDown={(event) => {
@@ -364,8 +162,8 @@ class MissingGarmentInfo extends React.Component {
                         <Label text="Hats" />
                         <Input
                             name="hats"
-                            value={this.state.hats}
-                            onChange={this.sizeInputChange}
+                            value={this.props.hats}
+                            onChange={this.props.handleInputChange}
                             type="number"
                             min={0}
                             onKeyDown={(event) => {
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -18,34 +18,157 @@ class Home extends Component {
     design: "",
     issue: "",
     reason: "",
-    topInput: false,
-    missingInput: false,
-    receivedInput: false,
-    missing: "",
-    received: ""
+    missing: {
+      brand: "",
+      style: "",
+      color: "",
+      xSmall: 0,
+      small: 0,
+      medium: 0,
+      large: 0,
+      xLarge: 0,
+      twoXL: 0,
+      threeXL: 0,
+      fourXL: 0,
+      fiveXL: 0,
+      hats: 0
+    },
+    received: {
+      brand: "",
+      style: "",
+      color: "",
+      xSmall: 0,
+      small: 0,
+      medium: 0,
+      large: 0,
+      xLarge: 0,
+      twoXL: 0,
+      threeXL: 0,
+      fourXL: 0,
+      fiveXL: 0,
+      hats: 0
+    },
+    POInfo: false,
+    missingInfo: false,
+    receivedInfo: false
   }
 
   handleInputChange = event => {
+
     const { name, value } = event.target;
-    console.log(name, value)
 
-    if (name === "PONum") {
-      let POVal = value.replace(/[^0-9]+/g, "")
-      this.setState({ PONum: POVal.trim() });
-    }
-    else {
-      this.setState({ [name]: value.trim() });
+    let numVal = parseInt(value)
+
+    let issue = this.state.issue
+
+    switch (name) {
+      case "PONum":
+        let POVal = value.replace(/[^0-9]+/g, "")
+        this.setState({ PONum: POVal.trim() });
+        break;
+      case "design":
+        this.setState({ design: value.trim() });
+        break;
+      case "brand":
+        this.setState(prevState => {
+          let missing = Object.assign({}, prevState.missing);
+          missing.brand = value.trim()
+          return { missing }
+        })
+        break;
+      case "style":
+        this.setState(prevState => {
+          let missing = Object.assign({}, prevState.missing);
+          missing.style = value.trim()
+          return { missing }
+        })
+        break;
+      case "color":
+        let colorVal = value.replace(/[^a-zA-Z ]/g, "")
+        this.setState(prevState => {
+          let missing = Object.assign({}, prevState.missing);
+          missing.color = colorVal.trim()
+          return { missing }
+        })
+        break;
+      case "xSmall":
+        this.setState(prevState => {
+          let missing = Object.assign({}, prevState.missing);
+          missing.xSmall = numVal
+          return { missing }
+        })
+        break;
+      case "small":
+        this.setState(prevState => {
+          let missing = Object.assign({}, prevState.missing);
+          missing.small = numVal
+          return { missing }
+        })
+        break;
+      case "medium":
+        this.setState(prevState => {
+          let missing = Object.assign({}, prevState.missing);
+          missing.medium = numVal
+          return { missing }
+        })
+        break;
+      case "large":
+        this.setState(prevState => {
+          let missing = Object.assign({}, prevState.missing);
+          missing.large = numVal
+          return { missing }
+        })
+        break;
+      case "xLarge":
+        this.setState(prevState => {
+          let missing = Object.assign({}, prevState.missing);
+          missing.xLarge = numVal
+          return { missing }
+        })
+        break;
+      case "twoXL":
+        this.setState(prevState => {
+          let missing = Object.assign({}, prevState.missing);
+          missing.twoXL = numVal
+          return { missing }
+        })
+        break;
+      case "threeXL":
+        this.setState(prevState => {
+          let missing = Object.assign({}, prevState.missing);
+          missing.threeXL = numVal
+          return { missing }
+        })
+        break;
+      case "fourXL":
+        this.setState(prevState => {
+          let missing = Object.assign({}, prevState.missing);
+          missing.fourXL = numVal
+          return { missing }
+        })
+        break;
+      case "fiveXL":
+        this.setState(prevState => {
+          let missing = Object.assign({}, prevState.missing);
+          missing.fiveXL = numVal
+          return { missing }
+        })
+        break;
+      case "hats":
+        this.setState(prevState => {
+          let missing = Object.assign({}, prevState.missing);
+          missing.hats = numVal
+          return { missing }
+        })
+        break;
     }
 
-    if (this.state.PONum.length === 5 && this.state.design && this.state.issue) {
-      this.setState({ topInput: true })
-    }
-    else {
-      this.setState({ topInput: false })
+    switch (issue) {
+
     }
   };
 
-  issueDropDown = event => {
+  dropDownChange = event => {
     console.log(event)
     switch (event) {
       case "Missing Garments":
@@ -54,20 +177,6 @@ class Home extends Component {
       case "Extra Garments Received - Packing Slip is Correct, No Missing Garments":
         this.setState({ issue: event });
         break;
-    }
-  }
-
-  setMissingState = (missing) => {
-    this.setState({ missing: missing })
-  }
-
-  setReceivedState = (received) => {
-    this.setState({ missing: received })
-  }
-
-  reasonDropDown = event => {
-    console.log(event)
-    switch (event) {
       case "Overage - Extra Sizes Received":
         this.setState({ reason: event });
         break;
@@ -114,30 +223,91 @@ class Home extends Component {
               <Label text="Issue" />
               <DropdownList
                 name="issue"
-                onChange={this.issueDropDown}
+                onChange={this.dropDownChange}
                 data={issues}
               />
             </FormGroup>
             {
-              this.state.issue === "Missing Garments"
-                ?
-                <div>
-                  <h3>What is missing?</h3>
-                  <hr></hr>
-                  <MissingGarmentInfo
-                    setMissingState={this.setMissingState}
-                  />
-                  <FormBtn
-                    text="Next"
-                    classes="btn-success logoutBtn homeBtn"
-                  />
-                </div>
+              this.state.issue === "Missing Garments" ?
+                !this.state.reason ?
+                  <FormGroup>
+                    <Label text="Reason" />
+                    <DropdownList
+                      name="reason"
+                      onChange={this.dropDownChange}
+                      data={reasons}
+                    />
+                  </FormGroup>
+                  :
+                  <div>
+                    <FormGroup>
+                      <Label text="Reason" />
+                      <DropdownList
+                        name="reason"
+                        onChange={this.dropDownChange}
+                        data={reasons}
+                        value={this.state.reason}
+                      />
+                    </FormGroup>
+                    <h3>What is missing?</h3>
+                    <hr></hr>
+                    <MissingGarmentInfo
+                      handleInputChange={this.handleInputChange}
+                      brand={this.state.missing.brand}
+                      style={this.state.missing.style}
+                      color={this.state.missing.color}
+                      xSmall={this.state.missing.xSmall}
+                      small={this.state.missing.small}
+                      medium={this.state.missing.medium}
+                      large={this.state.missing.large}
+                      xLarge={this.state.missing.xLarge}
+                      twoXL={this.state.missing.twoXL}
+                      threeXL={this.state.missing.threeXL}
+                      fourXL={this.state.missing.fourXL}
+                      fiveXL={this.state.missing.fiveXL}
+                      hats={this.state.missing.hats}
+                    />
+                    <FormBtn
+                      text="Next"
+                      classes="btn-success logoutBtn homeBtn"
+                      // disabled={(!this.state.PONum || !this.state.design || !this.state.issue || !this.state.missing.brand || !this.state.missing.style || !this.state.missing.color) && (this.state.missing.xSmall === 0 && this.state.missing.small === 0 && this.state.missing.medium === 0 && this.state.missing.large === 0 && this.state.missing.xLarge === 0 && this.state.missing.twoXL === 0 && this.state.missing.threeXL === 0 && this.state.missing.fourXL === 0 && this.state.missing.fiveXL === 0) ? "" : "disabled"}
+                      disabled={true}
+                    />
+                  </div>
                 :
                 ""
+
+              // <div>
+              //   <h3>What is missing?</h3>
+              //   <hr></hr>
+              //   <MissingGarmentInfo
+              //     handleInputChange={this.handleInputChange}
+              //     brand={this.state.missing.brand}
+              //     style={this.state.missing.style}
+              //     color={this.state.missing.color}
+              //     xSmall={this.state.missing.xSmall}
+              //     small={this.state.missing.small}
+              //     medium={this.state.missing.medium}
+              //     large={this.state.missing.large}
+              //     xLarge={this.state.missing.xLarge}
+              //     twoXL={this.state.missing.twoXL}
+              //     threeXL={this.state.missing.threeXL}
+              //     fourXL={this.state.missing.fourXL}
+              //     fiveXL={this.state.missing.fiveXL}
+              //     hats={this.state.missing.hats}
+              //   />
+              //   <FormBtn
+              //     text="Next"
+              //     classes="btn-success logoutBtn homeBtn"
+              //     // disabled={(!this.state.PONum || !this.state.design || !this.state.issue || !this.state.missing.brand || !this.state.missing.style || !this.state.missing.color) && (this.state.missing.xSmall === 0 && this.state.missing.small === 0 && this.state.missing.medium === 0 && this.state.missing.large === 0 && this.state.missing.xLarge === 0 && this.state.missing.twoXL === 0 && this.state.missing.threeXL === 0 && this.state.missing.fourXL === 0 && this.state.missing.fiveXL === 0) ? "" : "disabled"}
+              //     disabled={true}
+              //   />
+              // </div>
+              // :
+              // ""
             }
             {
-              this.state.issue === "Extra Garments Received - Packing Slip is Correct, No Missing Garments"
-                ?
+              this.state.issue === "Extra Garments Received - Packing Slip is Correct, No Missing Garments" ?
                 <div>
                   <h3>What was Received?</h3>
                   <hr></hr>
